e2e4dab1112dde03e913f99bfa065e70d7fffb92
Andrey Markov <alyldas@ya.ru>
2019-03-29 02:03:06 +0300

    Bulma weather table test

--- src/index.js
+++ src/index.js
@@ -3,14 +3,47 @@ import ReactDOM from "react-dom";
 
 import "./styles.css";
 
+import "bulma/css/bulma.css";
+import ReactAnimatedWeather from "react-animated-weather";
+
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faLocationArrow } from "@fortawesome/free-solid-svg-icons";
+
 function App() {
   return (
-    <div className="App">
-      <h1>Hello CodeSandbox</h1>
-      <h2>Start editing to see some magic happen!</h2>
-    </div>
+    <section class="section">
+      <div class="container">
+        <table class="table is-fullwidth is-narrow">
+          <tr>
+            <td>
+              <strong>Сегодня</strong>
+              <br />
+              29 Марта
+            </td>
+            <td style={{ verticalAlign: "middle" }}>
+              <ReactAnimatedWeather icon="CLEAR_DAY" size="32" />
+            </td>
+            <td style={{ verticalAlign: "middle", textAlign: "right" }}>
+              <strong>2°</strong>
+            </td>
+            <td style={{ verticalAlign: "middle" }}>-1°</td>
+            <td
+              style={{
+                verticalAlign: "middle",
+                textAlign: "right",
+                paddingRight: 0
+              }}
+            >
+              <span class="icon">
+                <FontAwesomeIcon icon={faLocationArrow} size="xs" />
+              </span>
+            </td>
+            <td style={{ verticalAlign: "middle", paddingLeft: 0 }}>3 м/с</td>
+          </tr>
+        </table>
+      </div>
+    </section>
   );
 }
 
-const rootElement = document.getElementById("root");
-ReactDOM.render(<App />, rootElement);
+ReactDOM.render(<App />, document.getElementById("root"));
